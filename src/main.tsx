2f3c49c05477ff4c2ccb623c3f0789b1eb0f796e
Kastriot Çunaku <kastriotcunaku@gmail.com>
2024-10-08 09:15:47 +0200

    feat: add github source code link

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <main className="flex min-h-screen flex-col items-center justify-between md:p-24 p-6">
       <App />
+      <div className="text-center pt-3">
+        <a href="https://github.com/kastriotcunaku/secure-local-data-example" target="_blank">GitHub ↗</a>
+      </div>
     </main>
   </StrictMode>,
 )
